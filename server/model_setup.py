9a8a96543653def7b202b8cac63f86934d892602
Enrique <enriquec@mit.edu>
2022-05-06 21:16:28 -0400

    Cleaned model setup

--- server/model_setup.py
+++ server/model_setup.py
@@ -2,48 +2,22 @@ from nl4dv import NL4DV
 import os
 
 print(__file__)
-DATA_PATH = os.path.join("server", "assets", "data")
+DATA_PATH = os.path.join("server", "assets", "benchmark", "data")
 MODELS_PATH = os.path.join("server", "models")
 
 
 def get_nl4dv_instance():
-    # Change back to the root directory
-    # os.path.dirname(os.path.realpath(__file__))
-
-    # os.chdir(os.path.join(".."))
-    # print(os.path.dirname(os.path.realpath(__file__)))
-    # os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    # Initialize an instance of NL4DV
-    # ToDo: verify the path to the source data file. modify accordingly.
     nl4dv_instance = NL4DV(
         data_url=os.path.join(DATA_PATH, "cinema.csv"),
     )
 
-    # using Stanford Core NLP
-    # ToDo: verify the paths to the jars. modify accordingly.
-    # dependency_parser_config = {
-    #     "name": "corenlp",
-    #     "model": os.path.join(
-    #         MODELS_PATH,
-    #         "nl4dv",
-    #         "server-jars",
-    #         "stanford-english-corenlp-2018-10-05-models.jar",
-    #     ),
-    #     "parser": os.path.join(
-    #         MODELS_PATH,
-    #         "nl4dv",
-    #         "server-jars",
-    #         "stanford-parser.jar",
-    #     ),
-    # }
-
-    # using Stanford CoreNLPServer
-    # ToDo: verify the URL to the CoreNLPServer. modify accordingly.
-    # dependency_parser_config = {"name": "corenlp-server", "url": "http://localhost:9000"}
-
     # using Spacy
     # ToDo: ensure that the below spacy model is installed. if using another model, modify accordingly.
-    dependency_parser_config = {"name": "spacy", "model": "en_core_web_sm", "parser": None}
+    dependency_parser_config = {
+        "name": "spacy",
+        "model": "en_core_web_sm",
+        "parser": None,
+    }
 
     # Set the Dependency Parser
     nl4dv_instance.set_dependency_parser(config=dependency_parser_config)
